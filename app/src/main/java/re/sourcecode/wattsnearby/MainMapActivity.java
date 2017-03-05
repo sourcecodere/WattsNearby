b056a98a3cc30f5c11c6fb138dd686de57ff9437
sourcecodere <sourcecodere@gmail.com>
2017-03-05 21:54:43 +0100

    Empty Activity project with icons and drawables.

---
+++ app/src/main/java/re/sourcecode/wattsnearby/MainMapActivity.java
@@ -0,0 +1,13 @@
+package re.sourcecode.wattsnearby;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_map);
+    }
+}
