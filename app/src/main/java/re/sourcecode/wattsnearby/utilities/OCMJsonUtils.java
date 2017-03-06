2ee4d4f2ce0fae8094eee0509b8482b60f9f07c0
sourcecodere <sourcecodere@gmail.com>
2017-03-06 22:31:07 +0100

    Started OCM json filter

---
+++ app/src/main/java/re/sourcecode/wattsnearby/utilities/OCMJsonUtils.java
@@ -0,0 +1,82 @@
+package re.sourcecode.wattsnearby.utilities;
+
+import android.content.ContentValues;
+import android.content.Context;
+
+import org.json.JSONException;
+
+/**
+ * Created by olem on 3/6/17.
+ */
+
+public class OCMJsonUtils {
+
+
+    // http://api.openchargemap.io/v2/poi/?output=json&countrycode=NO&maxresults=10&latitude=60.029265&longitude=11.0952163&distanceunit=km&distance=2
+
+    /* Operator information */
+    private static final String OCM_OPERATOR = "OperatorInfo";
+    private static final String OCM_OPERATOR_TITLE = "Title";
+    private static final String OCM_OPERATOR_URL = "WebsiteURL";
+
+    /* Usage Type */
+    private static final String OCM_UT = "UsageType";
+    private static final String OCM_UT_PAY_ON_SITE = "IsPayAtLocation";
+    private static final String OCM_UT_MEMBERSHIP = "IsMembershipRequired";
+    private static final String OCM_UT_ACCESSKEY = "IsAccessKeyRequired";
+    private static final String OCM_UT_TITLE = "Title";
+
+    /* Address info */
+    private static final String OCM_ADDR = "AddressInfo";
+    private static final String OCM_ADDR_LATITUDE = "Latitude";
+    private static final String OCM_ADDR_LONGITUDE = "Longitude";
+    private static final String OCM_ADDR_DISTANCE = "Distance";
+    private static final String OCM_ADDR_TITLE = "Title";
+    private static final String OCM_ADDR_LINE1 = "AddressLine1";
+    private static final String OCM_ADDR_LINE2 = "AddressLine2";
+    private static final String OCM_ADDR_TOWN = "Town";
+    private static final String OCM_ADDR_STATE = "StateOrProvince";
+    private static final String OCM_ADDR_POSTCODE = "PostCode";
+    private static final String OCM_ADDR_COUNTRY = "Country";
+    private static final String OCM_ADDR_COUNTRY_ISO = "ISOCode";
+    private static final String OCM_ADDR_COUNTRY_TITLE = "Title";
+
+    /* Comments */
+    private static final String OCM_COMMENTS = "GeneralComments";
+
+    /* Status */
+    private static final String OCM_TIME_UPDATED = "DateLastStatusUpdate";
+
+    /* Connections */
+    private static final String OCM_CONNECTIONS = "Connections"; //LIST OF CONNECTIONS AT ADDRESS
+    private static final String OCM_CONNECTIONS_TYPE = "ConnectionType";
+    private static final String OCM_CONNECTIONS_TYPE_TITLE = "Title"; //e.g. CHAdeMO
+    private static final String OCM_CONNECTIONS_TYPE_ID = "ID"; //e.g. 2 is CHAdeMO,  33 CCS
+    private static final String OCM_CONNECTIONS_LEVEL_FAST = "IsFastChargeCapable";
+    private static final String OCM_CONNECTIONS_LEVEL_TITLE = "Title";
+    private static final String OCM_CONNECTIONS_AMP = "Amps";
+    private static final String OCM_CONNECTIONS_VOLT = "Voltage";
+    private static final String OCM_CONNECTIONS_KW = "PowerKW";
+    private static final String OCM_CONNECTIONS_CURRENT = "CurrentType";
+    private static final String OCM_CONNECTIONS_CURRENT_TITLE = "Title";
+
+    /**
+     * This method parses JSON from a web response and returns an array of Strings
+     * describing the charging stations in the current area
+     * <p/>
+     *
+     * @param forecastJsonStr JSON response from server
+     *
+     * @return Array of Strings describing OCM data
+     *
+     * @throws JSONException If JSON data cannot be properly parsed
+     */
+    public static ContentValues[] getOCMContentValuesFromJson(Context context, String ocmJsonStr)
+            throws JSONException {
+
+
+
+
+    }
+
+}
