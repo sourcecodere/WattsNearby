7bc1e6558725544d231d873bd1a7c9185808fce1
sourcecodere <sourcecodere@gmail.com>
2017-03-16 10:38:58 +0100

    Database with tables for storing jsondata.

---
+++ app/src/main/java/re/sourcecode/wattsnearby/data/OcmContract.java
@@ -0,0 +1,111 @@
+package re.sourcecode.wattsnearby.data;
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * Created by olem on 3/16/17.
+ */
+
+public class OcmContract {
+
+    /*
+    * The "Content authority" is a name for the entire content provider, similar to the
+    * relationship between a domain name and its website. A convenient string to use for the
+    * content authority is the package name for the app, which is guaranteed to be unique on the
+    * Play Store.
+    */
+    public static final String CONTENT_AUTHORITY = "re.sourcecode.wattsnearby;
+
+    /*
+     * Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+     * the content provider for Sunshine.
+     */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    /*
+     * Possible paths that can be appended to BASE_CONTENT_URI to form valid URI's that Wattsnearby
+     * can handle. For instance,
+     *
+     *     content://re.sourcecode.android.wattsnearby/station/
+     *     [           BASE_CONTENT_URI         ][ PATH_STATION ]
+     *
+     * is a valid path for looking at wattsnearby data.
+     *
+     *      content://re.sourcecode.android.wattsnearby/givemeroot/
+     *
+     * will fail, as the ContentProvider hasn't been given any information on what to do with
+     * "givemeroot". At least, let's hope not. Don't be that dev, reader. Don't be that dev.
+     */
+    public static final String PATH_STATION = "station";
+
+    /* Inner class that defines the table contents of the station table */
+    public static final class StationEntry implements BaseColumns {
+
+        /* The base CONTENT_URI used to query the Station table from the content provider */
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon()
+                .appendPath(PATH_STATION)
+                .build();
+
+        /* Used internally as the name of  table. */
+        public static final String TABLE_NAME = "station";
+
+        public static final String COLUMN_ID = "id";
+
+        /* Operator */
+        public static final String COLUMN_OPERATOR_TITLE = "operator_title";
+        public static final String COLUMN_OPERATOR_WEBSITE = "operator_website";
+
+        /* Usage Type */
+        public static final String COLUMN_UT_PAY_ON_SITE = "is_pay_at_location";
+        public static final String COLUMN_UT_MEMBERSHIP = "is_membership_required";
+        public static final String COLUMN_UT_ACCESSKEY = "is_access_key_required";
+        public static final String COLUMN_UT_TITLE = "usage_type_title";
+
+        /* Address/Location info */
+        public static final String COLUMN_LAT = "lat";
+        public static final String COLUMN_LON = "lat";
+        public static final String COLUMN_DISTANCE = "distance";
+        public static final String COLUMN_ADDR_TITLE = "addr_title";
+        public static final String COLUMN_ADDR_LINE1 = "addr_line1";
+        public static final String COLUMN_ADDR_LINE2 = "addr_line2";
+        public static final String COLUMN_ADDR_TOWN = "addr_town";
+        public static final String COLUMN_ADDR_STATE = "addr_state";
+        public static final String COLUMN_ADDR_POSTCODE = "addr_post_code";
+        public static final String COLUMN_ADDR_COUNTRY_ISO = "addr_country_iso";
+        public static final String COLUMN_ADDR_COUNTRY_TITLE = "addr_country";
+
+        /* Comments */
+        public static final String COLUMN_COMMENTS = "comments";
+
+        /* Status */
+        public static final String COLUMN_TIME_UPDATED = "time_updated";
+
+    }
+
+    /* ONE(station)-TO-MANY(Connections) */
+
+    /* Inner class that defines the table contents of the stations connection table */
+    public static final class ConnectionEntry implements BaseColumns {
+
+        /* Used internally as the name of table. */
+        public static final String TABLE_NAME = "connection";
+
+        public static final String COLUMN_ID = "id";
+
+        /* Connections */
+        public static final String COLUMN_CONN_TITLE = "con_title";
+        public static final String COLUMN_CONN_TYPE_ID = "con_type_id";
+        public static final String COLUMN_CONN_LEVEL_FAST = "con_level_fast";
+        public static final String COLUMN_CONN_LEVEL_TITLE = "con_level_title";
+        public static final String COLUMN_CONN_AMP = "con_amp";
+        public static final String COLUMN_CONN_VOLT = "con_volt";
+        public static final String COLUMN_CONN_KW = "con_kw";
+        public static final String COLUMN_CONN_CURRENT_TYPE_DESC = "con_current_type_desc";
+        public static final String COLUMN_CONN_CURRENT_TYPE_TITLE = "con_current_title";
+
+        public static final String COLUMN_CONN_STATION_ID = "con_station_id";
+
+    }
+
+}
--- app/src/main/java/re/sourcecode/wattsnearby/utilities/OcmJsonUtils.java
+++ app/src/main/java/re/sourcecode/wattsnearby/utilities/OcmJsonUtils.java
@@ -3,16 +3,21 @@ package re.sourcecode.wattsnearby.utilities;
 import android.content.ContentValues;
 import android.content.Context;
 
+import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by olem on 3/6/17.
  */
 
-public class OCMJsonUtils {
+public class OcmJsonUtils {
 
 
     // http://api.openchargemap.io/v2/poi/?output=json&countrycode=NO&maxresults=10&latitude=60.029265&longitude=11.0952163&distanceunit=km&distance=2
+
+    /* ID */
+    private static final String OCM_ID = "ID";
 
     /* Operator information */
     private static final String OCM_OPERATOR = "OperatorInfo";
@@ -51,7 +56,7 @@ public class OCMJsonUtils {
     private static final String OCM_CONNECTIONS = "Connections"; //LIST OF CONNECTIONS AT ADDRESS
     private static final String OCM_CONNECTIONS_TYPE = "ConnectionType";
     private static final String OCM_CONNECTIONS_TYPE_TITLE = "Title"; //e.g. CHAdeMO
-    private static final String OCM_CONNECTIONS_TYPE_ID = "ID"; //e.g. 2 is CHAdeMO,  33 CCS
+    private static final String OCM_CONNECTIONS_TYPE_ID = "ConnectionTypeID"; //e.g. 2 is CHAdeMO,  33 CCS
     private static final String OCM_CONNECTIONS_LEVEL_FAST = "IsFastChargeCapable";
     private static final String OCM_CONNECTIONS_LEVEL_TITLE = "Title";
     private static final String OCM_CONNECTIONS_AMP = "Amps";
@@ -65,14 +70,29 @@ public class OCMJsonUtils {
      * describing the charging stations in the current area
      * <p/>
      *
-     * @param forecastJsonStr JSON response from server
+     * @param ocmJsonStr JSON response from server
      *
-     * @return Array of Strings describing OCM data
+     * @return Array of ContentValues describing OCM data
      *
      * @throws JSONException If JSON data cannot be properly parsed
      */
     public static ContentValues[] getOCMContentValuesFromJson(Context context, String ocmJsonStr)
             throws JSONException {
+
+        JSONArray ocmJsonArray = new JSONArray(ocmJsonStr); //JSONobject from json string
+
+        ContentValues[] ocmContentValues = new ContentValues[ocmJsonArray.length()];
+
+        for(int i = 0; i < ocmJsonArray.length(); i++) {
+            JSONObject ocmJsonObj = ocmJsonArray.getJSONObject(i);
+
+
+
+        }
+
+
+
+
 
 
 
